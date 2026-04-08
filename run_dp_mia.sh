c2d9a285caae1ea965fb7ca8a7ce7a98c0d5ea1f
HoweChen517 <812868677@qq.com>
2026-04-08 14:07:22 +0800

    修改实验组、新增可视化

--- run_dp_mia.sh
+++ run_dp_mia.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-algo=("fedavg_dp_MIA" "fedprox_dp_MIA" "fedditto_dp_MIA")
+# algo=("fedditto_dp_MIA" "fedavg_dp_MIA" "fedprox_dp_MIA")
+algo=("fedavg_dp_MIA")
 dataset=("cifar10")
-partition=("iid" "shard")
+partition=("iid")
 
 GLOBAL_ROUNDS=50
-C=("0.5" "1.0" "5.0" "1e6")
-SIGMA=("0.0" "0.1" "0.5" "1.0")
-Q=("0.3" "0.5" "0.7" "1.0")
+C=("0.5" "1.0" "5.0")
+SIGMA=("0.1" "0.5" "1.0")
+Q=("0.2" "0.5" "0.8")
 
 for d in "${dataset[@]}"; do
     for a in "${algo[@]}"; do
@@ -23,6 +24,7 @@ for d in "${dataset[@]}"; do
                             --optimizer "dp_sgd" \
                             --device "mps" \
                             --global_rounds "$GLOBAL_ROUNDS" \
+                            --local_epochs 1 \
                             --dp_noise_multiplier "$s" \
                             --dp_sample_rate "$q" \
                             --dp_max_grad_norm "$c" 
